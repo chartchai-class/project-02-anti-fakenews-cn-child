170e8379c207088191567b37101f38934a60e96c
child-AZ <274170836@qq.com>
2025-11-28 18:41:53 +0800

    deloy and revise

--- backend/src/main/java/se331/lab/rest/config/SecurityConfig.java
+++ backend/src/main/java/se331/lab/rest/config/SecurityConfig.java
@@ -62,8 +62,8 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(
-                java.util.Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:5175"));
+        configuration.setAllowedOrigins(java.util.Arrays.asList("http://localhost:3000", "http://localhost:8080",
+                "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://119.91.69.247"));
         configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.Arrays.asList("*"));
         configuration.setAllowCredentials(true);
